3b807ace2c14f8e139691e732cbd61a50bf710f8
Anton Palgunov <Toxblh@users.noreply.github.com>
2024-09-27 00:33:11 +0100

    Update tampermonkey.js

--- tampermonkey.js
+++ tampermonkey.js
@@ -1,1 +1,33 @@
+// ==UserScript==
+// @name           Github-ish skin BugZilla
+// @name:ru        BugZilla скин под Github
+// @description    Modernized visual update for Alt Bugzilla.
+// @description:ru Предоставляет обновлённый современный визуальный стиль для Alt Bugzilla.
+// @grant          GM_addStyle
+// @grant          GM_xmlhttpRequest
+// @require        https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/md5.js
+// @require        https://raw.githubusercontent.com/Toxblh/Github-ish-skin-BugZilla/refs/heads/master/script.js
+// @match          https://bugzilla.altlinux.org/*
+// @namespace      altbugzilla
+// @version        1.0.0
+// @icon           https://bugzilla.altlinux.org/images/favicon.svg
+// @author         Toxblh
+// @homepageURL    https://github.com/Toxblh/Github-ish-skin-BugZilla
+// @updateURL      https://raw.githubusercontent.com/Toxblh/Github-ish-skin-BugZilla/refs/heads/master/tampermonkey.js
+// @downloadURL    https://raw.githubusercontent.com/Toxblh/Github-ish-skin-BugZilla/refs/heads/master/tampermonkey.js
+// @supportURL     https://github.com/Toxblh/Github-ish-skin-BugZilla/issues
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+    // Загружаем и применяем CSS файл
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: "https://raw.githubusercontent.com/Toxblh/Github-ish-skin-BugZilla/refs/heads/master/styles.css",
+        onload: function(response) {
+            GM_addStyle(response.responseText);
+        }
+    });
+
+})();
